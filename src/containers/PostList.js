1810d7c3cdb414cad9ec8e47dbe8b908b23648c1
Artus U <artusrock@hotmail.com>
2021-10-23 12:12:06 +0100

    useFetch custom hook

--- src/containers/PostList.js
+++ src/containers/PostList.js
@@ -1,54 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import {NavLink} from 'react-router-dom'
-import { Divider, Header, Item } from 'semantic-ui-react'
-import axios from 'axios'
-import Loader from '../components/Loader';
-import Message from '../components/Message'
-import { api } from '../api';
+import { NavLink } from "react-router-dom";
+import { Divider, Header, Item } from "semantic-ui-react";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { api } from "../api";
+import { useFetch } from "../helpers";
 
 const PostList = () => {
-    const [posts, setPosts] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null);
+  const { data, loading, error } = useFetch(api.posts.list);
 
-    useEffect(() => {
-        async function fetchData() {
-            setLoading(true);
-            try {
-                const res = await axios.get(api.posts.list);
-                setPosts(res.data)
-                setLoading(false)
-            } catch (error) {
-                setError(error.message)
-                setLoading(false)
-            }
-        }
-        fetchData();
-    }, [])
-
-    return (
-        <div>
-        <Header>Post list</Header>
-        <Divider/>
-        {error && <Message negative message={error} />}
-        {loading && <Loader />}
-        <Item.Group>
-            {posts?.map(post => {
-                return (
-                    <Item key={post.id}>
-                        <Item.Image size='small' src={post.thumbnail} />
-                        <Item.Content>
-                            <NavLink to={`/posts/${post.slug}`}>
-                                <Item.Header as='h3'>{post.title}</Item.Header>
-                            </NavLink>
-                            <Item.Description>{post.content}</Item.Description>
-                        </Item.Content>
-                    </Item>
-                )
-            })}
-        </Item.Group>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header>Post list</Header>
+      <Divider />
+      {error && <Message negative message={error} />}
+      {loading && <Loader />}
+      <Item.Group>
+        {data?.map((post) => {
+          return (
+            <Item key={post.id}>
+              <Item.Image size="small" src={post.thumbnail} />
+              <Item.Content>
+                <NavLink to={`/posts/${post.slug}`}>
+                  <Item.Header as="h3">{post.title}</Item.Header>
+                </NavLink>
+                <Item.Description>{post.content}</Item.Description>
+              </Item.Content>
+            </Item>
+          );
+        })}
+      </Item.Group>
+    </div>
+  );
+};
 
 export default PostList;
